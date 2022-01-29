c1c4c43094e78b50464cb80498f41ecdb9190d9e
David Carlson <dcarlson@clinicalcloud.solutions>
2022-01-28 19:35:10 -0700

    Updates

---
+++ precache-manifest.f3c5b994558f81bede786fa317efcfec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767db3729497cd173c478a18e6e27a01",
+    "url": "/mcc-patient-v2/index.html"
+  },
+  {
+    "revision": "28b93d60f91ea2017940",
+    "url": "/mcc-patient-v2/static/css/5.80cf868f.chunk.css"
+  },
+  {
+    "revision": "92d1b1fe7c5762e3f9aa",
+    "url": "/mcc-patient-v2/static/css/main.1316d802.chunk.css"
+  },
+  {
+    "revision": "30a2e3f773c0855a08bd",
+    "url": "/mcc-patient-v2/static/js/0.278a462f.chunk.js"
+  },
+  {
+    "revision": "28b93d60f91ea2017940",
+    "url": "/mcc-patient-v2/static/js/5.1a6d9dfc.chunk.js"
+  },
+  {
+    "revision": "74bcac65d7838b6201752fdd5f46bd53",
+    "url": "/mcc-patient-v2/static/js/5.1a6d9dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d46d6a04d54fbb5409da",
+    "url": "/mcc-patient-v2/static/js/launch.029dd7e7.chunk.js"
+  },
+  {
+    "revision": "92d1b1fe7c5762e3f9aa",
+    "url": "/mcc-patient-v2/static/js/main.31df7d90.chunk.js"
+  },
+  {
+    "revision": "b065afcc63ec18322fb5",
+    "url": "/mcc-patient-v2/static/js/runtime-launch.62f98f14.js"
+  },
+  {
+    "revision": "c53ecaf200db5c72b306",
+    "url": "/mcc-patient-v2/static/js/runtime-main.6ce75ed9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mcc-patient-v2/precache-manifest.30e9bd3f923b6e1cccaf7f45aab437dd.js"
+  "/mcc-patient-v2/precache-manifest.f3c5b994558f81bede786fa317efcfec.js"
 );
 
 self.addEventListener('message', (event) => {
