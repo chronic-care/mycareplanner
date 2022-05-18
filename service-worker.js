be59e923e2aa98a45accc91f30ac822ca34f8d16
David Carlson <dcarlson@clinicalcloud.solutions>
2022-05-18 10:49:27 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mycareplanner/precache-manifest.f80afb6a89025a92d402a1b9d3c2eb4a.js"
+  "/mycareplanner/precache-manifest.dcf31fd6f41a42bcde2fb88b340edb40.js"
 );
 
 self.addEventListener('message', (event) => {
