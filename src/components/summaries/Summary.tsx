293ef31f3e4c8f2170ff9c8e1d63cdf2d5878c1f
Dan Brown <dbrown.se@gmail.com>
2022-09-06 18:38:13 -0400

    Create modular MUI summary system

---
+++ src/components/summaries/Summary.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import Grid from '@mui/material/Grid'
+import { createTheme, ThemeProvider } from '@mui/material'
+import { SummaryRow } from './SummaryRow'
+
+interface SummaryRowItem {
+  isHeader: boolean,
+  twoColumns: boolean,
+  data1: any,
+  data2: any,
+}
+
+export interface SummaryRowItems extends Array<SummaryRowItem> { }
+
+interface SummaryProps {
+  id: number,
+  header?: string,
+  rows: SummaryRowItems,
+}
+
+const summaryTheme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "h6"
+          },
+          style: {
+            fontSize: 18,
+            fontWeight: "bold",
+          },
+        },
+        {
+          props: {
+            variant: "body2"
+          },
+          style: {
+            fontSize: "14px !important",
+          },
+        }
+      ]
+    }
+  }
+})
+
+export const Summary: React.FC<SummaryProps> = (props: SummaryProps) => {
+  return (
+    <ThemeProvider theme={summaryTheme}>
+      <Paper key={props.id} elevation={4} style={{ padding: 8, marginBottom: 13 }}>
+        <Grid container spacing={1}>
+
+          {props.rows?.map((row, idx) => (
+            <SummaryRow key={idx} isHeader={row.isHeader} twoColumns={row.twoColumns}
+              data1={row.data1} data2={row.data2} />
+          ))}
+
+        </Grid>
+      </Paper>
+    </ThemeProvider>
+  )
+}
---
+++ src/components/summaries/SummaryRow.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+
+interface SummaryRowProps {
+  isHeader: boolean,
+  twoColumns: boolean,
+  data1?: string | undefined,
+  data2?: string | Element | undefined,
+}
+
+export const SummaryRow: React.FC<SummaryRowProps> = (props: SummaryRowProps) => {
+
+  if (!props.twoColumns) {
+    return (
+      <Grid item xs={12}>
+        <Typography variant={props.isHeader ? "h6" : "body2"} component={props.isHeader ? "h6" : "p"}>
+          {props.data1}
+        </Typography>
+      </Grid>
+    )
+  } else {
+    return (
+      <>
+        <Grid item xs={6}>
+          <Typography variant={props.isHeader ? "h6" : "body2"} component={props.isHeader ? "h6" : "p"}>
+            {props.data1}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="body2" component="p" align="right">
+            {props.data2}
+          </Typography>
+        </Grid>
+      </>
+    )
+  }
+
+}
