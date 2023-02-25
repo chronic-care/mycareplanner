ec727832424092d833ff34a6f919efbbd094bfaf
David Carlson <dcarlson@clinicalcloud.solutions>
2023-02-24 19:04:57 -0700

    Update ProviderLogin to include Cerner and VA sanboxes

--- src/components/shared-data/ProviderLogin.tsx
+++ src/components/shared-data/ProviderLogin.tsx
@@ -20,6 +20,8 @@ export default function ProviderLogin() {
   let history = useHistory()
 
   const epicScope = "launch launch/patient openid fhirUser patient/Patient.read patient/Practitioner.read patient/RelatedPerson.read patient/Condition.read patient/DiagnosticReport.read patient/Observation.read patient/Procedure.read patient/CarePlan.read patient/CareTeam.read patient/Goal.read patient/Immunization.read patient/MedicationRequest.read patient/Medication.read patient/Provenance.read patient/Organization.read";
+  const cernerScope = "launch/patient openid fhirUser offline_access patient/Patient.read user/Practitioner.read user/Location.read user/Organization.read patient/CarePlan.read patient/CareTeam.read patient/Condition.read patient/Goal.read patient/Immunization.read patient/Observation.read patient/Procedure.read patient/MedicationRequest.read patient/RelatedPerson.read patient/ServiceRequest.read patient/Provenance.read";
+  const vaScope = "launch/patient openid profile offline_access patient/Patient.read patient/Practitioner.read patient/Condition.read patient/Observation.read patient/Immunization.read patient/MedicationRequest.read patient/Medication.read";
 
   const endpoints: ProviderEndpoint[] = [
     {
@@ -50,30 +52,30 @@ export default function ProviderLogin() {
       }
     },
     {
-      name: 'Vanderbilt',
-      config: {
-        iss: "https://arr01.service.vumc.org/FHIR-PRD/api/FHIR/R4/",
-        redirectUri: "./index.html",
-        clientId: process.env.REACT_APP_CLIENT_ID_epic,
-        scope: epicScope
-      }
-    },
-    {
-      name: 'Walmart Health',
-      config: {
-        iss: "https://epicinterconnect.walmarthealth.com/Interconnect-OAuth2-PRD/api/FHIR/R4/",
-        redirectUri: "./index.html",
-        clientId: process.env.REACT_APP_CLIENT_ID_epic,
-        scope: epicScope
-      }
-    },
-    {
       name: 'Epic Sandbox (test data)',
       config: {
         iss: "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4",
         redirectUri: "./index.html",
         clientId: process.env.REACT_APP_CLIENT_ID_epic_sandbox,
         scope: epicScope
+      }
+    },
+    {
+      name: 'Cerner Sandbox (test data)',
+      config: {
+        iss: "https://fhir-myrecord.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d",
+        redirectUri: "./index.html",
+        clientId: process.env.REACT_APP_CLIENT_ID_cerner_sandbox,
+        scope: cernerScope
+      }
+    },
+    {
+      name: 'VA Sandbox (test data)',
+      config: {
+        iss: "https://sandbox-api.va.gov/services/fhir/v0/r4",
+        redirectUri: "./index.html",
+        clientId: process.env.REACT_APP_CLIENT_ID_va,
+        scope: vaScope
       }
     }
   ]
