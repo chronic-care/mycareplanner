5b67ad63e35a33d47931041f4605e7fbf7dd2c0b
David Carlson <dcarlson@clinicalcloud.solutions>
2024-03-28 11:57:11 -0600

    Updates

--- precache-manifest.48d496c8d5797fabcadf8a7ebbdd25c8.js
+++ precache-manifest.48d496c8d5797fabcadf8a7ebbdd25c8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa5a39d374902ced2902cf589e571736",
+    "revision": "43813dcc8ccd49b735c519505b05fe62",
     "url": "/mycareplanner/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mycareplanner/static/css/5.3fac59f9.chunk.css"
   },
   {
-    "revision": "1736660b9b3c6068ed1e",
+    "revision": "02af111ba34ff43b7ed7",
     "url": "/mycareplanner/static/css/main.34f55237.chunk.css"
   },
   {
@@ -32,12 +32,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mycareplanner/static/js/launch.f874ae60.chunk.js"
   },
   {
-    "revision": "1736660b9b3c6068ed1e",
-    "url": "/mycareplanner/static/js/main.aba9ece9.chunk.js"
+    "revision": "02af111ba34ff43b7ed7",
+    "url": "/mycareplanner/static/js/main.03602afc.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/mycareplanner/static/js/main.aba9ece9.chunk.js.LICENSE.txt"
+    "url": "/mycareplanner/static/js/main.03602afc.chunk.js.LICENSE.txt"
   },
   {
     "revision": "6dd3e02d2b8a498f38c4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mycareplanner/precache-manifest.be71979b7ca21abc1fd59085d427d62e.js"
+  "/mycareplanner/precache-manifest.48d496c8d5797fabcadf8a7ebbdd25c8.js"
 );
 
 self.addEventListener('message', (event) => {
