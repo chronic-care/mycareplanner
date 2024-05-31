38a7670b8ebaa58ff1a3b395d1ca931ae1e05a42
Sean <>
2024-05-31 12:38:47 -0400

    Updates for releae 2.2.1
    Modified version
    removed health quesitonaire
    removed cancel from quesitonaire

--- src/Home.tsx
+++ src/Home.tsx
@@ -147,11 +147,8 @@ export default class Home extends React.Component<HomeProps, HomeState> {
               pathname: '/questionnaire',
               state: { patientSummaries: this.props.patientSummaries, questionnaireId: 'caregiver-strain-questionnaire' }
             }} ><strong>Caregiver Strain Assessment</strong></Link><br />
-            <Link to={{
-              pathname: '/questionnaire',
-              state: { patientSummaries: this.props.patientSummaries, questionnaireId: 'patient-priorities-care-questionnaire' }
-            }} ><strong>My Health Priorities</strong></Link><br />
 
+         
             {/* {(tasks === undefined)
                 ? <p>You have no tasks today!</p>
                 : <ul>
