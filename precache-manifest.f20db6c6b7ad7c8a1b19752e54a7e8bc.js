face945054a86f255626d58fa5f7674f0dea8ac4
David Carlson <dcarlson@clinicalcloud.solutions>
2024-07-30 15:01:16 -0600

    Updates

---
+++ precache-manifest.f20db6c6b7ad7c8a1b19752e54a7e8bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fd3c13b791e8dc3c803cd5b53f92f5",
+    "url": "/mycareplanner/index.html"
+  },
+  {
+    "revision": "3b342e62b781e806f9f2",
+    "url": "/mycareplanner/static/css/5.3fac59f9.chunk.css"
+  },
+  {
+    "revision": "683184b4126f46677bb6",
+    "url": "/mycareplanner/static/css/main.1b11033f.chunk.css"
+  },
+  {
+    "revision": "8a6bd38a9df1a101047a",
+    "url": "/mycareplanner/static/js/0.dba9a3eb.chunk.js"
+  },
+  {
+    "revision": "7cf95e8f5d516e303331e05e43d106af",
+    "url": "/mycareplanner/static/js/0.dba9a3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b342e62b781e806f9f2",
+    "url": "/mycareplanner/static/js/5.d9223896.chunk.js"
+  },
+  {
+    "revision": "1ca9b68204a972567e20ea8f0b72314d",
+    "url": "/mycareplanner/static/js/5.d9223896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b4c7bfad2e5d86f84a",
+    "url": "/mycareplanner/static/js/launch.b48c59aa.chunk.js"
+  },
+  {
+    "revision": "683184b4126f46677bb6",
+    "url": "/mycareplanner/static/js/main.4ca7a0dc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/mycareplanner/static/js/main.4ca7a0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd3e02d2b8a498f38c4",
+    "url": "/mycareplanner/static/js/runtime-launch.d80f08ae.js"
+  },
+  {
+    "revision": "6c559e5647c64a61dac1",
+    "url": "/mycareplanner/static/js/runtime-main.69118ab0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mycareplanner/precache-manifest.98df536318b1f08843371e3adc50c854.js"
+  "/mycareplanner/precache-manifest.f20db6c6b7ad7c8a1b19752e54a7e8bc.js"
 );
 
 self.addEventListener('message', (event) => {
